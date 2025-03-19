7ede205a8b615af0df0e5cd0657dcda031ba247d
mac của sduy <sonduy436@gmail.com>
2025-03-19 10:32:46 +0700

    hieu gay

--- uetoasis/tuan3/Student.java
+++ uetoasis/tuan3/Student.java
@@ -41,8 +41,25 @@ public class Student {
         this.name = name;
     }
 
+    public String getID() {
+        return id;
+    }
+    public void setID(String id) {
+        this.id = id;
+    }
+
     public String getGroup() {
         return group;
+    }
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getInfo() {
--- uetoasis/tuan3/StudentManagement.java
+++ uetoasis/tuan3/StudentManagement.java
@@ -1,15 +1,19 @@
 package uetoasis.tuan3;
 
 public class StudentManagement {
-    
+    private Student[] students = new Student[100];
+    private int count = 0;
 
     public static boolean sameGroup(Student s1, Student s2) {
-        String s1group = s1.getGroup();
-        String s2group = s2.getGroup();
-        if (s1group == s2group) {
-            return true;
-        } else {
-            return false;
+        return s1.getGroup().equals(s2.getGroup());
+    }
+    
+    public void addStudent(Student newStudent) {
+        if (this.count < 100) {
+            this.students[this.count] = newStudent;
+            ++this.count;
         }
     }
+
+    
 }
