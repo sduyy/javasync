02b6f1a2a1ab92bb6b464905a559dc4155b05c56
aspire của sduy <sonduy436@gmail.com>
2025-03-19 11:01:16 +0700

    nice

--- lam_devuet_bang_java/tuan3/NHCau6.java
+++ lam_devuet_bang_java/tuan3/NHCau6.java
@@ -1,20 +1,20 @@
-package lam_devuet_bang_java.tuan3;
+// package lam_devuet_bang_java.tuan3;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+// import java.time.LocalDate;
+// import java.time.format.DateTimeFormatter;
+// import java.util.Scanner;
 
-public class NHCau6 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String s = sc.nextLine();
-        try {
-            LocalDate date = LocalDate.parse(s, formatter);
-            System.out.println("yes");
-        } catch (Exception e) {
-            System.out.println("no");
-        }
-        sc.close();
-    }
-}
+// public class NHCau6 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//         String s = sc.nextLine();
+//         try {
+//             LocalDate date = LocalDate.parse(s, formatter);
+//             System.out.println("yes");
+//         } catch (Exception e) {
+//             System.out.println("no");
+//         }
+//         sc.close();
+//     }
+// }
