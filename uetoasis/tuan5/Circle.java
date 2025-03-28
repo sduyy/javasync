566c1ab6effea8705ed93e72a49116233ca692e2
mac của sduy <sonduy436@gmail.com>
2025-03-28 09:58:22 +0700

    n

---
+++ uetoasis/tuan5/Circle.java
@@ -0,0 +1,69 @@
+public class Circle {
+    private double radius;
+    private String color;
+    protected static final double PI = 3.14;
+
+    /**
+     * This is constructor.
+     */
+    public Circle() {
+
+    }
+
+    /**
+     * This is constructor.
+     */
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * This is constructor.
+     */
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    /**
+     * This is to get radius.
+     */
+    public double getRadius() {
+        return radius;
+    }
+
+    /**
+     * This is to set radius.
+     */
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * This is to get color.
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * This is to set color.
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * This is to get area.
+     */
+    public double getArea() {
+        return PI * radius * radius;
+    }
+
+    /**
+     * This is to string.
+     */
+    public String toString() {
+        return "Circle [radius=" + radius + ", color=" + color + "]";
+    }
+}
---
+++ uetoasis/tuan5/Cylinder.java
@@ -0,0 +1,70 @@
+public class Cylinder extends Circle {
+    private double height;
+
+    /**
+     * This is constructor.
+     */
+    public Cylinder() {
+
+    }
+
+    /**
+     * This is constructor.
+     */
+    public Cylinder(double height) {
+
+    }
+
+    /**
+     * This is constructor.
+     */
+    public Cylinder(double height, double radius) {
+        super(radius);
+        this.height = height;
+    }
+
+    /**
+     * This is constructor.
+     */
+    public Cylinder(double height, double radius, String color) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    /**
+     * This is to get height.
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * This is to set height.
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    /**
+     * This is to get volume.
+     */
+    public double getVolume() {
+        return getArea() * getHeight();
+    }
+
+    /**
+     * This is to string.
+     */
+    @Override
+    public String toString() {
+        return "Cylinder[" + super.toString() + "height=" + height + "]";
+    }
+
+    /**
+     * This is to get area.
+     */
+    @Override
+    public double getArea() {
+        return getArea() + getArea() + getRadius() * height;
+    }
+}
