32d0d2ad4ed7451b9c190824458f6ee7b516d589
mac của sduy <sonduy436@gmail.com>
2025-05-07 10:27:44 +0700

    n

--- uetoasis/tuan9/CoVua/Piece.java
+++ uetoasis/tuan9/CoVua/Piece.java
@@ -5,46 +5,76 @@ public abstract class Piece {
     private int coordinatesY;
     private String color;
 
+    /**
+     * This is constructor.
+     */
     public Piece(int coordinatesX, int coordinatesY) {
         this.coordinatesX = coordinatesX;
         this.coordinatesY = coordinatesY;
     }
 
+    /**
+     * This is constructor.
+     */
     public Piece(int coordinatesX, int coordinatesY, String color) {
         this.coordinatesX = coordinatesX;
         this.coordinatesY = coordinatesY;
         this.color = color;
     }
 
+    /**
+     * This is to get.
+     */
     public abstract String getSymbol();
 
+    /**
+     * This is to check.
+     */
     public abstract boolean canMove(Board board, int x, int y);
 
+    /**
+     * This is to get.
+     */
     public int getCoordinatesX() {
         return this.coordinatesX;
     }
 
+    /**
+     * This is to set.
+     */
     public void setCoordinatesX(int coordinatesX) {
         this.coordinatesX = coordinatesX;
     }
 
+    /**
+     * This is to get.
+     */
     public int getCoordinatesY() {
         return this.coordinatesY;
     }
 
+    /**
+     * This is to set.
+     */
     public void setCoordinatesY(int coordinatesY) {
         this.coordinatesY = coordinatesY;
     }
 
+    /**
+     * This is to get.
+     */
     public String getColor() {
         return this.color;
     }
 
+    /**
+     * This is to set.
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
     public boolean checkPosition(Piece piece) {
-        
+
     }
 }
--- uetoasis/tuan9/CoVua/Rook.java
+++ uetoasis/tuan9/CoVua/Rook.java
@@ -1,19 +1,45 @@
 package uetoasis.tuan9.CoVua;
 
+import java.util.ArrayList;
+
 public class Rook extends Piece{
+    /**
+     * This is constructor.
+     */
     public Rook(int coordinatesX, int coordinatesY) {
         super(coordinatesX, coordinatesY);
     }
 
+    /**
+     * This is constructor.
+     */
     public Rook(int coordinatesX, int coordinatesY, String color) {
         super(coordinatesX, coordinatesY, color);
     }
 
+    /**
+     * This is to get.
+     */
     public String getSymbol() {
         return "R";
     }
 
+    /**
+     * This is to check.
+     */
     public boolean canMove(Board board, int x, int y) {
-        
+        boolean check = true;
+        if ((x == this.getCoordinatesX() && y != this.getCoordinatesY()) || (x != this.getCoordinatesX() && y == this.getCoordinatesY())) {
+            ArrayList<Piece> pieces = board.getPieces();
+            for (Piece p : pieces) {
+                if (x == p.getCoordinatesX() && y == p.getCoordinatesY() && this.getColor() == p.getColor()) {
+                    check = false;
+                    break;
+                }
+            }
+        } else {
+            return false;
+        }
+        return check;
     }
 }
